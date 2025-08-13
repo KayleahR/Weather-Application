af0ffabf586eddf918110d76cad83cef8e317a8e
Kayleah <mx.ryoshi@gmail.com>
2025-08-12 20:09:16 -0600

    api for city injected

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
-function showWeatherData(response) {
-  let temperature = response.data.temperature;
-  let description = response.data.condition.description;
-  let city = response.data.city;
-
+function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
-  let descriptionElement = document.querySelector("#current-description");
-  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = `${response.data.temperature.current}°C`;
 
-  temperatureElement.innerHTML = `${temperature}°C`;
-  descriptionElement.innerHTML = description;
-  cityElement.innerHTML = city;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 }
 
-function searchCCity (city) {
+function searchCity(city) {
   let apiKey = "co26tbed7b33e4ff46f2bdb0b88af4ea";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showWeatherData);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
